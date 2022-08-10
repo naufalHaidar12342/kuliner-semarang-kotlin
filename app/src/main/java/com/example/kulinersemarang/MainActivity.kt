a50c8f1db817ed40d9ca5f3255975be746c22467
naufalHaidar12342 <111201912342@mhs.dinus.ac.id>
2022-08-10 22:01:36 +0700

    menambahkan toolbar ke main activity

--- app/src/main/java/com/example/kulinersemarang/MainActivity.kt
+++ app/src/main/java/com/example/kulinersemarang/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.kulinersemarang
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kulinersemarang.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bindingMainActivity:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bindingMainActivity= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingMainActivity.root)
+
+        setSupportActionBar(bindingMainActivity.materialToolbar)
+
+
+
     }
 }
